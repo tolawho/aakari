abd672cafc007764b34e24a50075ab07b404ccd0
Vasanth Developer <vasanthdevelopers@gmail.com>
2018-11-09 23:08:18 +0530

    SASS compilation now works
    1. Created aakari.scss file
    2. Added gulp-sass and node-sass as dependencies
    3. Added gulp-sourcemaps as dependency
    4. Added gulp-autoprefixer and gulp-csso as dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,32 @@
-var gulp = require('gulp');
+'use strict';
+
+const gulp = require('gulp'),
+	  sass = require('gulp-sass'),
+	  sourcemaps = require('gulp-sourcemaps'),
+	  autoprefixer = require('gulp-autoprefixer'),
+	  csso = require('gulp-csso');
+
+// Setting the sass compiler as node-sass
+sass.compiler = require('node-sass');
+
+// Setting SASS compiler settings
+var SassOptions = {
+	indentedSyntax: true,
+	indentType: 'tab',
+	indentWidth: 4,
+	outputStyle: 'expanded'
+};
 
 // The Gulp task for preparing CSS
+gulp.task('sass-compile', function() {
+	return gulp.src('./sources/sass/**/*.+(scss|sass)')
+		.pipe(sourcemaps.init())
+		.pipe(sass(SassOptions).on('error', sass.logError))
+		.pipe(autoprefixer({browsers: ['last 4 versions']}))
+		.pipe(csso())
+		.pipe(sourcemaps.write('./maps'))
+		.pipe(gulp.dest('./assets/css'));
+});
 
 // Gulp Task for optimizing JavaScripts
 
