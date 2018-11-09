112990ba9ba029dc7396baf211e383189d3b5235
Vasanth Developer <vasanthdevelopers@gmail.com>
2018-11-09 22:28:56 +0530

    Added Gulpfile
    1. Added Gulp.js as dependency
    2. Added Bootstrap 4 as dependency

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+// The Gulp task for preparing CSS
+
+// Gulp Task for optimizing JavaScripts
+
+// Gulp task for packaging the theme into a ZIP file
+
+// The default Gulp task which first does a fresh build and watches for any changes
