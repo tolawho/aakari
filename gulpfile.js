d22cffdd49444c35126c3b1d7e2fa97e31c5aba5
Vasanth Developer <vasanthdevelopers@gmail.com>
2018-11-10 21:01:52 +0530

    Added JqueryLazy as dependency
    1. Added jquery.lazy.min.js to gulpfile on install-javascripts task

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,8 @@ var SassOptions = {
 var ReqJavaScripts = [
 	'./node_modules/jquery/dist/jquery.min.js',
 	'./node_modules/popper.js/dist/umd/popper.min.js',
-	'./node_modules/bootstrap/dist/js/bootstrap.min.js'
+	'./node_modules/bootstrap/dist/js/bootstrap.min.js',
+	'./node_modules/jquery-lazy/jquery.lazy.min.js'
 ];
 
 // Defining unnecessary files for packaging
