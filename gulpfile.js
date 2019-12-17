bfaca22f901e965fbbfcb79fc913a8115984d51d
Vasanth Developer <vasanth@vasanthdeveloper.com>
2019-12-17 19:10:20 +0530

    🛠 Styles are now purged
    1. Added gulp-size and @fullhuman/postcss-purgecss as dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,22 @@ const del = require('del');
 const gulp = require('gulp');
 const zip = require('gulp-zip');
 const sass = require('gulp-sass');
+const size = require('gulp-size');
 const uglify = require('gulp-uglify');
 const postcss = require('gulp-postcss');
 const livereload = require('gulp-livereload');
 const sourcemaps = require('gulp-sourcemaps');
+const purgecss = require('@fullhuman/postcss-purgecss');
 
 const postCSSConfig = require('./postcss.config');
 
 // Setting the sass compiler as node-sass
 sass.compiler = require('node-sass');
+
+// The variable that holds options for gulp-size
+const sizeOptions = {
+    showFiles: true
+};
 
 // The variable that holds the paths/globs to different files
 const globs = {
@@ -43,31 +50,47 @@ const globs = {
 
 // The function that compiles SCSS and passes the CSS through PostCSS
 function styles() {
+    // Set the title for gulp-size
+    sizeOptions.title = 'styles';
+
     return gulp.src(globs.styles)
         .pipe(sourcemaps.init())
         .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
         .pipe(postcss(postCSSConfig))
         .pipe(sourcemaps.write('./maps'))
+        .pipe(size(sizeOptions))
         .pipe(gulp.dest('./assets/css'));
 }
 
 // The function that minifies JavaScript files
 function scripts() {
+    // Set the title for gulp-size
+    sizeOptions.title = 'scripts';
+
     return gulp.src(globs.scripts)
         .pipe(uglify())
+        .pipe(size(sizeOptions))
         .pipe(gulp.dest('./assets/js'));
 }
 
 // The function that installs vendor JavaScript
 function vendor_scripts() {
+    // Set the title for gulp-size
+    sizeOptions.title = 'vendor scripts';
+
     return gulp.src(globs.vendorScripts)
+        .pipe(size(sizeOptions))
         .pipe(gulp.dest('./assets/js'));
 }
 
 // The function that packages the theme into a zip file for installation in Ghost
 function package_theme() {
+    // Set the title for gulp-size
+    sizeOptions.title = 'package';
+
     return gulp.src(globs.unFilesForPack)
         .pipe(zip(globs.packageFilename))
+        .pipe(size(sizeOptions))
         .pipe(gulp.dest(globs.distDir));
 }
 
