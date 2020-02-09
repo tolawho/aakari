07f4b0e67174b25e09bed135a07fcac4002b29ae
Vasanth Developer <vasanth@vasanthdeveloper.com>
2020-02-10 00:59:48 +0530

    🍏 Ghost's bookmark cards now render
    1. Added tailwindcss as dependency
    2. Removed unnecessary Bootstrap imports
    3. Disabled scrollbar hiding CSS (Should be enabled later)

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+        extend: {},
+        boxShadow: {
+            default: '-1px 19px 26px -11px rgba(0,0,0,0.1)'
+        },
+        fontFamily: {
+            'display': ['Poppins', 'sans-serif']
+        }
+    },
+    variants: {},
+    plugins: []
+};
